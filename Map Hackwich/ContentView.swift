6bef9dfdae39db098d77fe620e5de5823449c154
Tom Bredemeier <tbredemeier@barrington220.org>
2026-02-02 11:41:08 -0600

    Added a location to the map

--- Map Hackwich/ContentView.swift
+++ Map Hackwich/ContentView.swift
@@ -11,13 +11,35 @@ import MapKit
 struct ContentView: View {
     @State private var locationManager = LocationManager()
     @State private var startPosition = MapCameraPosition.userLocation(fallback: .automatic)
+    @State private var places = [Place(name: "Barrington High School",
+                        coordinate: CLLocationCoordinate2D(
+                            latitude: 42.1565, longitude: -88.1480))]
     var body: some View {
         Map(position: $startPosition) {
             UserAnnotation()
+            ForEach(places) { place in
+                Annotation(place.name, coordinate: place.coordinate) {
+                    Image(systemName: "star.circle")
+                        .resizable()
+                        .foregroundStyle(.red)
+                        .frame(width: 44, height: 44)
+                        .background(.white)
+                        .clipShape(.circle)
+                }
+
+            }
+
+
         }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct Place: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
 }
