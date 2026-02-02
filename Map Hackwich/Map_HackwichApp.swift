0599b07e0d71409adb77247054ab6636dbb06461
Tom Bredemeier <tbredemeier@barrington220.org>
2026-02-02 10:29:34 -0600

    Initial Commit

---
+++ Map Hackwich/Map_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_HackwichApp.swift
+//  Map Hackwich
+//
+//  Created by Tom Bredemeier on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Map_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
